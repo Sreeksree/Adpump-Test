e3b7b3ac4fc3ac4c92cfa43be43aa52be7fdcd4b
Sreenath Kalarickal <kalarickalsreenath@gmail.com>
2025-03-26 22:05:13 +0530

    Update Main.java

--- src/com/client/Main.java
+++ src/com/client/Main.java
@@ -2,11 +2,13 @@ package com.client;
 
 public class Main {
     public static void main(String[] args) {
-        ProxyClient client = new ProxyClient();
-        //Sequential request
-//        client.addRequest("http://google.com");
-//        client.addRequest("http://fb.com");
-//        client.addRequest("http://twitter.com");
-        client.startListening();
+//        ProxyClient client = new ProxyClient();
+//        //Sequential request
+////        client.addRequest("http://google.com");
+////        client.addRequest("http://fb.com");
+////        client.addRequest("http://twitter.com");
+//        client.startListening();
+
+        new ProxyClient();
     }
 }
